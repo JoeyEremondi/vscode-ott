7d5f73af9230ccbcd130273d874308e231ce26c7
Joey Eremondi <joey.eremondi@gmail.com>
2018-02-01 22:38:25 -0800

    Move formatter to its own branch

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 // src/extension.ts
 import * as vscode from 'vscode';
 
-import { OttLintingProvider, OttFormatter } from './features/ottProvider';
+import { OttLintingProvider, } from './features/ottProvider';
 
 export function activate(context: vscode.ExtensionContext) {
     let linter = new OttLintingProvider();
     linter.activate(context.subscriptions);
     vscode.languages.registerCodeActionsProvider('ott', linter);
 }
-
-vscode.languages.registerDocumentFormattingEditProvider('ott', OttFormatter.ottFormatter);
 
